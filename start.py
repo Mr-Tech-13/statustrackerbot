ee5cb3a7b603b7b0cbc07e7420faf15ab0efa47a
Mr-Tech-13 <maxxshearey@hotmail.com>
2023-05-15 08:02:41 -0400

    Fix potential issue with this command

--- start.py
+++ start.py
@@ -67,13 +67,13 @@ async def error(ctx, title, description):
     channel = ctx.channel
     await channel.edit(name='\N{LARGE YELLOW CIRCLE} Service-Status')
     await ctx.channel.purge()
-    embed = discord.Embed(title='Service Status', description=':orange_circle: Online with errors, see below', color=0xFFFF00)
-    embed.set_author(name='*This embed updates when the service status changes*')
-    message = await ctx.send(embed=embed)
-    embed = discord.Embed(title=':yellow_circle: Caution', description=message, color=0xFFFF00)
+    embeda = discord.Embed(title='Service Status', description=':orange_circle: Online with errors, see below', color=0xFFFF00)
+    embeda.set_author(name='*This embed updates when the service status changes*')
+    message = await ctx.send(embed=embeda)
+    embed = discord.Embed(title=':yellow_circle: Caution', description=messagea, color=0xFFFF00)
     await ctx.send(embed=embed)
-    embed = discord.Embed(title=title, description=description, color=0xFFFF00)
-    message = await ctx.send(embed=embed)
+    embedb = discord.Embed(title=title, description=description, color=0xFFFF00)
+    messagea = await ctx.send(embed=embedb)
     global embed_message_id
     embed_message_id = message.id
     with open('message_id.txt', 'w') as f:
